570066f6160e7b512a192d5b1444a77b1ad8ead9
Burbia77 <lopezrellan@gmail.com>
2023-10-02 11:56:08 +0200

    Añadida clase rueda

---
+++ GestorCoches/src/es/netmind/simuladorcoches/modelos/ruedas/Rueda.java
@@ -0,0 +1,18 @@
+package es.netmind.simuladorcoches.modelos.ruedas;
+
+public class Rueda {
+    private Long diametro;
+
+    public Rueda(Long diametro) {
+
+    }
+
+    public Long getDiametro() {
+        return diametro;
+    }
+
+    public void setDiametro(Long diametro) {
+        this.diametro = diametro;
+    }
+}
+
